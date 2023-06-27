18808908665acc37ca625c8056a5dfef8ed245a6
wngus24311 <wngus24311@gmail.com>
2023-06-27 22:31:37 +0900

    진짜 오랜만이다 디아블로 때문에 아무것도 못하고 성역에 갇혀있었다... 오랜만에 업데이트 해서 올림!

--- src/main/java/com/forman/theathletic/controller/MainController.java
+++ src/main/java/com/forman/theathletic/controller/MainController.java
@@ -1,7 +1,15 @@
 package com.forman.theathletic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class MainController {
+
+    @GetMapping(value = "main")
+    public String home() {
+        return "index";
+    }
 }
